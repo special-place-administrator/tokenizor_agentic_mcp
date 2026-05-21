3360c5b9635323a235d0f790e1f104d0a7364fc8
special-place-administrator <197073597+special-place-administrator@users.noreply.github.com>
2026-05-21 21:48:28 +0200

    test: add systems backend inline extractor coverage

--- src/parsing/inline_tests.rs
+++ src/parsing/inline_tests.rs
@@ -33,3 +33,64 @@ macro_rules! inline_test {
 }
 
 pub(crate) use inline_test;
+
+#[cfg(test)]
+mod systems_backend_tests {
+    use crate::domain::{LanguageId, SymbolKind};
+
+    inline_test!(
+        go_inline_test_extracts_function,
+        LanguageId::Go,
+        r#"
+package main
+
+func InlineGoProbe() {}
+"#,
+        [(SymbolKind::Function, "InlineGoProbe")]
+    );
+
+    inline_test!(
+        java_inline_test_extracts_class,
+        LanguageId::Java,
+        r#"
+public class InlineJavaProbe {}
+"#,
+        [(SymbolKind::Class, "InlineJavaProbe")]
+    );
+
+    inline_test!(
+        c_inline_test_extracts_function,
+        LanguageId::C,
+        r#"
+int inline_c_probe(void) { return 0; }
+"#,
+        [(SymbolKind::Function, "inline_c_probe")]
+    );
+
+    inline_test!(
+        cpp_inline_test_extracts_function,
+        LanguageId::Cpp,
+        r#"
+int inline_cpp_probe() { return 0; }
+"#,
+        [(SymbolKind::Function, "inline_cpp_probe")]
+    );
+
+    inline_test!(
+        csharp_inline_test_extracts_class,
+        LanguageId::CSharp,
+        r#"
+public class InlineCSharpProbe {}
+"#,
+        [(SymbolKind::Class, "InlineCSharpProbe")]
+    );
+
+    inline_test!(
+        swift_inline_test_extracts_class,
+        LanguageId::Swift,
+        r#"
+class InlineSwiftProbe {}
+"#,
+        [(SymbolKind::Class, "InlineSwiftProbe")]
+    );
+}
